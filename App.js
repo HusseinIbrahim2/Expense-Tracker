3549354f3eaa27e4354f1ed80bac5eae731a79e8
DarkLord239 <husseinib23920@gmail.com>
2022-09-29 15:44:53 +0300

    Adding Bottom Tabs

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import AllExpensesScreen from './screens/AllExpensesScreen';
+import RecentExpensesScreen from './screens/RecentExpensesScreen';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={RecentExpensesScreen} />
+          <Tab.Screen name="Settings" component={AllExpensesScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
