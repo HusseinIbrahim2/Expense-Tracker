a172585d4f92bbff5383ce0e141fae2957e2e5ac
DarkLord239 <husseinib23920@gmail.com>
2022-09-29 15:34:57 +0300

    Create the Expenses Screens

---
+++ screens/AllExpensesScreen.js
@@ -0,0 +1,9 @@
+import { View , Text } from "react-native";
+
+function AllExpensesScreen() {
+    return (
+        <Text>This is the All Expenses Screen</Text>
+    )
+} 
+
+export default AllExpensesScreen;
---
+++ screens/RecentExpensesScreen.js
@@ -0,0 +1,9 @@
+import { View , Text } from "react-native";
+
+function RecentExpensesScreen() {
+    return (
+        <Text>This is the Recent Expenses Screen</Text>
+    )
+} 
+
+export default RecentExpensesScreen;
