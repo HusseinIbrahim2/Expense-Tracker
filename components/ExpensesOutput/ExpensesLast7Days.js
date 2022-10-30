074d9ec943df8c3c70eab1818d4bb48ed14cd9ff
DarkLord239 <husseinib23920@gmail.com>
2022-10-30 10:58:12 +0200

    Create ExpensesLast7Days component

---
+++ components/ExpensesOutput/ExpensesLast7Days.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+
+function ExpensesLast7Days() {
+    return (
+        <View>
+            <Text></Text>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default ExpensesLast7Days;
