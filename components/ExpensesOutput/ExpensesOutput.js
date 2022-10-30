688cd8de679df437d294f88d81acb8ee60fcbb37
DarkLord239 <husseinib23920@gmail.com>
2022-10-30 11:01:45 +0200

    Create ExpensesOutput component

---
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -0,0 +1,11 @@
+import ExpensesLast7Days from "./ExpensesLast7Days";
+import ExpensesList from "./ExpensesList";
+
+function ExpensesOutput() {
+    <>
+    <ExpensesLast7Days />
+    <ExpensesList />
+    </>
+}
+
+export default ExpensesOutput;
