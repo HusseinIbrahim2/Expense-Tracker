d1d1eb3d105a9bdb5e3aef7f02432850bd35dcaf
DarkLord239 <husseinib23920@gmail.com>
2022-10-30 21:21:52 +0200

    Styling ExpensesTotal output

--- components/ExpensesOutput/ExpensesTotal.js
+++ components/ExpensesOutput/ExpensesTotal.js
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 function ExpensesTotal({ period, expenses }) {
     const expensesSum = expenses.reduce((sum, expense) => {
@@ -6,11 +6,25 @@ function ExpensesTotal({ period, expenses }) {
     }, 0);
 
     return (
-        <View>
-            <Text>{period}</Text>
-            <Text>€{expensesSum.toFixed(2)}</Text>
+        <View style={styles.outerontainer}>
+            <View style={styles.innerContainer}>
+                <Text>{period}</Text>
+                <Text>€{expensesSum.toFixed(2)}</Text>
+            </View>
         </View>
     )
 }
-
+const styles = StyleSheet.create({
+    outerontainer: {
+        marginTop: 20,
+        margin: 15,
+        borderRadius: 5,
+        backgroundColor: 'lightgreen'
+    },
+    innerContainer: {
+        justifyContent: 'space-between',
+        flexDirection: 'row',
+        padding: 8,
+    }
+})
 export default ExpensesTotal;
