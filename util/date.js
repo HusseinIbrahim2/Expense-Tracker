6b07c021108463b2282804174fdbcf7731a3c9e0
DarkLord239 <husseinib23920@gmail.com>
2022-11-04 01:06:23 +0200

    Last 7 Days Expenses function

--- util/date.js
+++ util/date.js
@@ -3,3 +3,9 @@ export function getDate(date) {
      `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`
  )
 }
+
+export function getDateMinusDays(date , days ) {
+    return (
+        new Date(date.getFullYear() , date.getMonth() ,days.getDate() - days )
+    )
+}
