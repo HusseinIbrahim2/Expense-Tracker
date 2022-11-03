8fc5f41c20956cd995e51da08ed8eda8a51d5193
DarkLord239 <husseinib23920@gmail.com>
2022-11-04 00:42:13 +0200

    Set initial value to Reducer

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -4,38 +4,6 @@ import { View, StyleSheet } from "react-native";
 
 function ExpensesOutput({ expensesPeriod, totalExpenses }) {
 
-    const Expenses_Data = [
-        {
-            id: 'e1',
-            description: "one kilo of tomato ",
-            amount: 2,
-            date: new Date("2022-05-07")
-        },
-        {
-            id: 'e2',
-            description: "J.R.R.Tolkeins books  ",
-            amount: 850,
-            date: new Date("2018-02-09")
-        },
-        {
-            id: 'e3',
-            description: "Pen",
-            amount: 0.5,
-            date: new Date("2021-12-22")
-        },
-        {
-            id: 'e4',
-            description: "Iphone 14",
-            amount: 2399,
-            date: new Date("2022-10-20")
-        },
-        {
-            id: 'e5',
-            description: "chair",
-            amount: 30,
-            date: new Date("2022-10-12")
-        },
-    ];
     return (
         <View style={styles.container}>
             <ExpensesTotal period={expensesPeriod} expenses={Expenses_Data} />
--- store/Expenses-context.js
+++ store/Expenses-context.js
@@ -1,5 +1,38 @@
 import { createContext, useReducer } from "react";
 
+const Expenses_Data = [
+    {
+        id: 'e1',
+        description: "one kilo of tomato ",
+        amount: 2,
+        date: new Date("2022-05-07")
+    },
+    {
+        id: 'e2',
+        description: "J.R.R.Tolkeins books  ",
+        amount: 850,
+        date: new Date("2018-02-09")
+    },
+    {
+        id: 'e3',
+        description: "Pen",
+        amount: 0.5,
+        date: new Date("2021-12-22")
+    },
+    {
+        id: 'e4',
+        description: "Iphone 14",
+        amount: 2399,
+        date: new Date("2022-10-20")
+    },
+    {
+        id: 'e5',
+        description: "chair",
+        amount: 30,
+        date: new Date("2022-10-12")
+    },
+];
+
 export const ExpensesContext = createContext({
     expenses: [],
     addExpense: ({ description, amount, date }) => { },
@@ -24,11 +57,10 @@ function ExpensesReducer(state, action) {
         default:
             return state;
     }
-
 };
 
 function ExpensesContextProvider({ children }) {
-    const [expensesState, dispatch] = useReducer(ExpensesReducer, expensesState);
+    const [expensesState, dispatch] = useReducer(ExpensesReducer, Expenses_Data);
 
     function addExpense(expensesData) {
         dispatch({ type: 'ADD', payload: expensesData })
@@ -40,8 +72,15 @@ function ExpensesContextProvider({ children }) {
         dispatch({ type: 'UPDATE', id: id, payload: expensesData })
     }
 
+    const value = {
+        expenses: expensesState,
+        addExpense: addExpense,
+        deleteExpense: deleteExpense,
+        updateExpense: updateExpense,
+    };
+
     return (
-        <Managed.Provider>{children}</Managed.Provider>
+        <Managed.Provider value={value} >{children}</Managed.Provider>
     )
 }
 export default ExpensesContextProvider;
